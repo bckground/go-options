1bcec6a72cb6335f6e3db3c791565132f4789f69
João Fernandes <joao@bckground.com>
2025-12-19 01:19:20 +0000

    test: use namespaces

--- test/sample_test.go
+++ test/sample_test.go
@@ -40,12 +40,12 @@ var _ = Describe("Generating options", func() {
 	It("generates options to set config value", func() {
 		myInt := 456
 		err := applyConfigOptions(&cfg,
-			OptionMyInt(123),
-			OptionMyFloat(4.56),
-			OptionMyString("my-string"),
-			OptionMyIntPointer(&myInt),
-			OptionMyInterface(789),
-			OptionMyFunc(func() int { return 0 }),
+			OptionNs.MyInt(123),
+			OptionNs.MyFloat(4.56),
+			OptionNs.MyString("my-string"),
+			OptionNs.MyIntPointer(&myInt),
+			OptionNs.MyInterface(789),
+			OptionNs.MyFunc(func() int { return 0 }),
 		)
 		Ω(err).ShouldNot(HaveOccurred())
 		Ω(cfg.myInt).Should(Equal(123))
@@ -56,7 +56,7 @@ var _ = Describe("Generating options", func() {
 	})
 
 	It("generates an new function create a config", func() {
-		cfg, err := newConfig(OptionMyInt(123))
+		cfg, err := newConfig(OptionNs.MyInt(123))
 		Ω(err).ShouldNot(HaveOccurred())
 		Ω(cfg.myInt).Should(Equal(123))
 	})
@@ -70,12 +70,12 @@ var _ = Describe("Generating options", func() {
 	})
 
 	It("compares using standard equality", func() {
-		Ω(OptionMyInt(1)).Should(Equal(OptionMyInt(1)))
+		Ω(OptionNs.MyInt(1)).Should(Equal(OptionNs.MyInt(1)))
 	})
 
 	It("generates a String method", func() {
-		Ω(fmt.Sprintf("%v", OptionMyInt(1))).Should(Equal("OptionMyInt: 1"))
-		Ω(fmt.Sprintf("%v", OptionMyString("abc"))).Should(Equal("OptionMyString: abc"))
+		Ω(fmt.Sprintf("%v", OptionNs.MyInt(1))).Should(Equal("MyInt: 1"))
+		Ω(fmt.Sprintf("%v", OptionNs.MyString("abc"))).Should(Equal("MyString: abc"))
 	})
 
 	It("returns errors", func() {
@@ -84,7 +84,7 @@ var _ = Describe("Generating options", func() {
 	})
 
 	It("allows option constructor to be renamed", func() {
-		err := applyConfigOptions(&cfg, OptionYourInt(1))
+		err := applyConfigOptions(&cfg, OptionNs.YourInt(1))
 		Ω(err).ShouldNot(HaveOccurred())
 		Ω(cfg.myRenamedInt).To(Equal(1))
 	})
@@ -104,13 +104,13 @@ var _ = Describe("Generating options", func() {
 
 	Describe("imports", func() {
 		It("works with imported types", func() {
-			err := applyConfigOptions(&cfg, OptionMyDuration(time.Second))
+			err := applyConfigOptions(&cfg, OptionNs.MyDuration(time.Second))
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myDuration).Should(Equal(time.Second))
 		})
 
 		It("works with aliased imports", func() {
-			err := applyConfigOptions(&cfg, OptionMyDuration2(time.Second))
+			err := applyConfigOptions(&cfg, OptionNs.MyDuration2(time.Second))
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myDuration).Should(Equal(time.Second))
 		})
@@ -118,7 +118,7 @@ var _ = Describe("Generating options", func() {
 		It("works with nested packages", func() {
 			myURL, err := url.Parse("http://example.com")
 			Ω(err).ShouldNot(HaveOccurred())
-			err = applyConfigOptions(&cfg, OptionMyURL(*myURL))
+			err = applyConfigOptions(&cfg, OptionNs.MyURL(*myURL))
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myURL).Should(Equal(*myURL))
 		})
@@ -130,20 +130,20 @@ var _ = Describe("Generating options", func() {
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myPointerToInt).Should(BeNil())
 
-			err = applyConfigOptions(&cfg, OptionMyPointerToInt(1))
+			err = applyConfigOptions(&cfg, OptionNs.MyPointerToInt(1))
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myPointerToInt).ShouldNot(BeNil())
 			Ω(*cfg.myPointerToInt).Should(Equal(1))
 		})
 
 		It("generates a String method", func() {
-			Ω(fmt.Sprintf("%v", OptionMyPointerToInt(1))).Should(Equal("OptionMyPointerToInt: 1"))
+			Ω(fmt.Sprintf("%v", OptionNs.MyPointerToInt(1))).Should(Equal("MyPointerToInt: 1"))
 		})
 	})
 
 	Describe("nested structs", func() {
 		It("generates a constructor", func() {
-			err := applyConfigOptions(&cfg, OptionMyStruct(1, 2))
+			err := applyConfigOptions(&cfg, OptionNs.MyStruct(1, 2))
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myStruct.a).Should(Equal(1))
 			Ω(cfg.myStruct.b).Should(Equal(2))
@@ -160,7 +160,7 @@ var _ = Describe("Generating options", func() {
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myPointerToStruct).Should(BeNil())
 
-			err = applyConfigOptions(&cfg, OptionMyPointerToStruct(1, 2))
+			err = applyConfigOptions(&cfg, OptionNs.MyPointerToStruct(1, 2))
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myPointerToStruct).ShouldNot(BeNil())
 			Ω(cfg.myPointerToStruct.a).Should(Equal(1))
@@ -168,26 +168,26 @@ var _ = Describe("Generating options", func() {
 		})
 
 		It("allows variadic arguments within a slice", func() {
-			err := applyConfigOptions(&cfg, OptionMyStructWithVariadicSlice(1, 1, 2))
+			err := applyConfigOptions(&cfg, OptionNs.MyStructWithVariadicSlice(1, 1, 2))
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myStructWithDefault.a).Should(Equal(1))
 		})
 
 		It("generates a String method", func() {
-			Ω(fmt.Sprintf("%v", OptionMyStructWithVariadicSlice(1, 2))).Should(
-				Equal("OptionMyStructWithVariadicSlice: {a:1 b:[2]}"))
+			Ω(fmt.Sprintf("%v", OptionNs.MyStructWithVariadicSlice(1, 2))).Should(
+				Equal("MyStructWithVariadicSlice: {a:1 b:[2]}"))
 		})
 
 		It("allows variadic arguments to be compared with cmp", func() {
 			cmp.Equal(
-				OptionMyStructWithVariadicSlice(1, 1, 2),
-				OptionMyStructWithVariadicSlice(1, 1, 2))
+				OptionNs.MyStructWithVariadicSlice(1, 1, 2),
+				OptionNs.MyStructWithVariadicSlice(1, 1, 2))
 		})
 	})
 
 	Describe("variadic slices", func() {
 		It("creates a variadic constructor", func() {
-			err := applyConfigOptions(&cfg, OptionMySlice(1, 2))
+			err := applyConfigOptions(&cfg, OptionNs.MySlice(1, 2))
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.mySlice).Should(ConsistOf(1, 2))
 		})
@@ -197,27 +197,27 @@ var _ = Describe("Generating options", func() {
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myPointerToSlice).Should(BeNil())
 
-			err = applyConfigOptions(&cfg, OptionMyPointerToSlice(1, 2))
+			err = applyConfigOptions(&cfg, OptionNs.MyPointerToSlice(1, 2))
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myPointerToSlice).ShouldNot(BeNil())
 			Ω(*cfg.myPointerToSlice).Should(ConsistOf(1, 2))
 		})
 
 		It("allows them to be renamed", func() {
-			err := applyConfigOptions(&cfg, OptionYourSlice(1, 2))
+			err := applyConfigOptions(&cfg, OptionNs.YourSlice(1, 2))
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(cfg.myRenamedSlice).ShouldNot(BeNil())
 			Ω(cfg.myRenamedSlice).Should(ConsistOf(1, 2))
 		})
 
 		It("generates a String method", func() {
-			Ω(fmt.Sprintf("%v", OptionMySlice(1, 2))).Should(Equal("OptionMySlice: [1 2]"))
+			Ω(fmt.Sprintf("%v", OptionNs.MySlice(1, 2))).Should(Equal("MySlice: [1 2]"))
 		})
 
 		It("allows them to the to be compared with cmp", func() {
 			Ω(cmp.Equal(
-				OptionMySlice(1, 2),
-				OptionMySlice(1, 2))).Should(BeTrue())
+				OptionNs.MySlice(1, 2),
+				OptionNs.MySlice(1, 2))).Should(BeTrue())
 		})
 	})
 })
@@ -233,14 +233,14 @@ var _ = Describe("Customizing the apply function name", func() {
 
 var _ = Describe("Customizing the option prefix", func() {
 	It("creates options with the custom prefix", func() {
-		_, err := newConfigWithDifferentPrefix(OptMyFloat(1.23))
+		_, err := newConfigWithDifferentPrefix(MyOptNs.MyFloat(1.23))
 		Ω(err).ShouldNot(HaveOccurred())
 	})
 })
 
 var _ = Describe("Customizing the option suffix", func() {
 	It("creates options with the custom prefix", func() {
-		_, err := newConfigWithSuffix(MyFloatOption(1.23))
+		_, err := newConfigWithSuffix(SuffixOptionNs.MyFloat(1.23))
 		Ω(err).ShouldNot(HaveOccurred())
 	})
 })
@@ -255,20 +255,20 @@ var _ = Describe("not quoting strings by default", func() {
 
 var _ = Describe("Disabling cmp", func() {
 	It("prevents options from implementing Equal", func() {
-		_, equalsFound := reflect.TypeOf(NoCmpOptionMyInt(1)).MethodByName("Equal")
+		_, equalsFound := reflect.TypeOf(NoCmpOptionNs.MyInt(1)).MethodByName("Equal")
 		Ω(equalsFound).Should(BeFalse())
 	})
 })
 
 var _ = Describe("Disabling stringer", func() {
 	It("prevents options from implementing String", func() {
-		Ω(fmt.Sprintf("%v", NoStringerOptionMyInt(1))).Should(Equal("{1}"))
+		Ω(fmt.Sprintf("%v", NoStringerOptionNs.MyInt(1))).Should(Equal("{1}"))
 	})
 })
 
 var _ = Describe("Public new function", func() {
 	It("Makes the new confic cunction public", func() {
-		cfg, err := NewConfigWithPublicNewFunc(PublicFuncOptionMyInt(10))
+		cfg, err := NewConfigWithPublicNewFunc(PublicFuncOptionNs.MyInt(10))
 		Ω(err).ShouldNot(HaveOccurred())
 		Ω(cfg.myInt).Should(Equal(10))
 	})
